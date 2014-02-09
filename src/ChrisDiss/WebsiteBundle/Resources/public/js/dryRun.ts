de15ef0164955f44fd2104082f720e5988fd553f
Malte Wunsch <malte.wunsch@gmail.com>
2014-02-09 18:30:38 +0100

    TypeScripted controller files.

---
+++ src/ChrisDiss/WebsiteBundle/Resources/public/js/dryRun.ts
@@ -0,0 +1,51 @@
+/**
+ * AngularJS-controller for the dry run.
+ */
+function DryRunCtrl($scope, $timeout) {
+    'use strict';
+
+    /**
+     * Base controller with extracted commonalities between the dryRun- and test-Controller,
+     *
+     * @type {BaseController}
+     */
+    $scope.baseController = new BaseController(10, 2000, 1000, 3000, 3000, 50, true);
+
+    /**
+     * Manage the quiz control flow.
+     */
+    $scope.manageQuiz = function () {
+        var delayForDisplayingEvaluation = $scope.manageAnswerEvaluation();
+
+        if ($scope.quizShouldEnd() === true) {
+            $scope.baseController.manageEndOfQuestions();
+        } else {
+            $scope.baseController.manageNextQuestion($timeout, delayForDisplayingEvaluation, $scope);
+        }
+    };
+
+    /**
+     * Manage the evaluation of the user's answer: for the dry run, simply display it.
+     *
+     * @returns {number} delay for displaying the answer evaluation in milliseconds.
+     */
+    $scope.manageAnswerEvaluation = function () {
+        if ($scope.baseController.currentQuestionNumber === 1) {
+            return 0;
+        }
+
+        $scope.baseController.displayAnswerEvaluation($timeout, 0);
+        return $scope.baseController.durationOfAnswerEvaluationInMilliseconds;
+    };
+
+    /**
+     * Get whether one of the termination conditions is met.
+     *
+     * @returns {boolean}
+     */
+    $scope.quizShouldEnd = function () {
+        return $scope.baseController.currentQuestionNumber > $scope.baseController.numberOfQuestions;
+    };
+
+    $scope.manageQuiz();
+}
---
+++ src/ChrisDiss/WebsiteBundle/Resources/public/js/test.ts
@@ -0,0 +1,141 @@
+/**
+ * AngularJS-controller for the real test.
+ */
+function TestCtrl($scope, $timeout, $http) {
+    'use strict';
+
+    /**
+     * Number of Questions the user has been too slow to answer after which they will be given a "too slow" notice.
+     *
+     * @type {number}
+     */
+    $scope.numberOfNoAnswersBeforeNotice = 3;
+
+    /**
+     * Number of wrong answers (marked and unmarked) after which the real test run terminates.
+     *
+     * @type {number}
+     */
+    $scope.enoughWrongAnswersGiven = 20;
+
+    /**
+     * Base controller with extracted commonalities between the dryRun- and test-Controller,
+     *
+     * @type {BaseController}
+     */
+    $scope.baseController = new BaseController(100, 2000, 200, 1100, 3000, 80, false);
+
+    /**
+     * Aggregation of the AnswerEvaluations.
+     *
+     * @type {TestResult}
+     */
+    $scope.testResult = new TestResult();
+
+    /**
+     * Manage the quiz control flow.
+     */
+    $scope.manageQuiz = function () {
+        var delayForDisplayingEvaluation = $scope.manageAnswerEvaluation();
+
+        if ($scope.quizShouldEnd() === true) {
+            $scope.saveResult();
+            $scope.baseController.manageEndOfQuestions();
+        } else {
+            $scope.baseController.manageNextQuestion($timeout, delayForDisplayingEvaluation, $scope);
+        }
+    };
+
+    /**
+     * Manage the evaluation of the user's answer: For the real test run, store the evaluation in the test result and
+     * display the "too slow" message if the user has been too slow several times.
+     *
+     * @returns {number} delay for displaying the answer evaluation in milliseconds.
+     */
+    $scope.manageAnswerEvaluation = function () {
+        var delayForDisplayingEvaluation = 0,
+            answerEvaluation;
+
+        if ($scope.baseController.currentQuestionNumber === 1) {
+            return delayForDisplayingEvaluation;
+        }
+
+        answerEvaluation = $scope.baseController.getAnswerEvaluation();
+        $scope.testResult.add(answerEvaluation);
+
+        // letzte answer no answer und y no answers schon da
+        if (answerEvaluation instanceof NoAnswerEvaluation && this.testResult.getNumberOfNoAnswers() > this.numberOfNoAnswersBeforeNotice) {
+            $scope.baseController.displayAnswerEvaluation($timeout, 0);
+            delayForDisplayingEvaluation += $scope.baseController.durationOfAnswerEvaluationInMilliseconds;
+        }
+
+        return delayForDisplayingEvaluation;
+    };
+
+    /**
+     * Get whether one of the termination conditions is met.
+     *
+     * @returns {boolean}
+     */
+    $scope.quizShouldEnd = function () {
+        var allQuestionsAsked = $scope.baseController.currentQuestionNumber > $scope.baseController.numberOfQuestions,
+            enoughWrongAnswersGiven = $scope.testResult.getSumOfWrongAnswers() >= $scope.enoughWrongAnswersGiven;
+
+        return allQuestionsAsked || enoughWrongAnswersGiven;
+    };
+
+    /**
+     * Save the test result via an AJAX call.
+     */
+    $scope.saveResult = function () {
+        $http(
+            {
+                method: 'POST',
+                url: $scope.getUrlForSavingResult(),
+                data: $scope.getPostDataForSavingResult(),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }
+        ).success(
+            function(data, status, headers, config) {
+            }
+        ).error(
+            function(data, status, headers, config) {
+                alert('Leider ist ein Fehler aufgetreten. (Fehlercode: ' + status + ')');
+            }
+        );
+    }
+
+    /**
+     * Get the URL for saving the test result.
+     *
+     * @returns {string}
+     */
+    $scope.getUrlForSavingResult = function () {
+        var urlForSavingResult,
+            currentPath = window.location.pathname;
+
+        if (currentPath.charAt(0) !== '/') {
+            currentPath = '/' + currentPath;
+        }
+        urlForSavingResult = currentPath.substr(0, currentPath.indexOf('/testung')) + '/save-result';
+
+        return urlForSavingResult;
+    };
+
+    /**
+     * Get the data for saving the result as a string, suitable for the request body.
+     *
+     * @returns {string}
+     */
+    $scope.getPostDataForSavingResult = function () {
+        return $.param({
+            'markedWrongAnswers' : $scope.testResult.getNumberOfMarkedWrongAnswers(),
+            'unmarkedWrongAnswers': $scope.testResult.getNumberOfUnmarkedWrongAnswers(),
+            'markedCorrectAnswers': $scope.testResult.getNumberOfMarkedCorrectAnswers(),
+            'unmarkedCorrectAnswers': $scope.testResult.getNumberOfUnmarkedCorrectAnswers(),
+            'noAnswers': $scope.testResult.getNumberOfNoAnswers()
+        });
+    };
+
+    $scope.manageQuiz();
+}
